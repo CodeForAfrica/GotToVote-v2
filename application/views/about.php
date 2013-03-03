090e679acadc5d9add6dcb7434189a59a58c4a40
Nick Hargreaves <hargreaves1234@gmail.com>
2013-03-03 14:49:49 +0300

    Updated 'About' page

--- application/views/about.php
+++ application/views/about.php
@@ -4,19 +4,33 @@
         			<div class="page-header">
         				<h2>About </h2>
         			</div>
-        			
-        			<!--<p class="lead"><b>GotToVote!</b> is designed to help Kenyans find their nearest voter registration centre, so that they can enroll themselves as registered voters and be eligible to vote in the 2013 general election.</p>
-        			-->
-        			<p><b>GotToVote!</b> is an example of how open data can be useful to ordinary citizens.</p>
-        			
-        			<p><b>GotToVote!</b> was built at virtually zero cost as a <a href="http://code4kenya.org">Code4Kenya</a> data journalism project. It demonstrates how data-driven tools can help media audiences act on the news they read / watch, by finding out how a national event such as the elections affects their personal lives or local communities.</p>
-        			
-        			<p><a href="http://code4kenya.org">Code4Kenya</a> is a pilot programme funded by the <a href="http://africanmediainitiative.org">Africa Media Initiative</a> through the Digital Projects Division. It embedded four Data Fellows into major Kenyan newsrooms and a civil society organisation, for five months, to help kickstart experimentation with data-driven civic engagement tools. The Data Fellows were supported by an external software development team. The program is implemented by the <a href="http://openinstitute.com">Open Institute</a>.</p>
-        			
-        			<p><a href="http://code4kenya.org">Code4Kenya</a> has inspired similar initiatives in Ghana and South Africa. Its success has prompted AMI to pledge additional funding to allow Code4Kenya's jAccelerator lab to continue building civic tech projects until the end of 2013.</p>
-        			
-        			<p>All the data used on <b>GotToVote!</b> is freely available on another Code4Kenya project: the <a href="http://africaopendata.org">AfricaOpenData</a> portal, which is the continent's largest repository of public data, ranging from government budget and tender information, to data about parliamentarians and other public officials. The original data was extracted from a pdf provided by the <a href="http://iebc.or.ke">Independent Electoral and Boundaries Commission (IEBC)</a>.</p>
-        			
+       <p> 			
+      <b>GotToVote</b>! is an example of how open data can be useful to ordinary citizens.
+</p>
+<p>
+<b>GotToVote</b>! was built as a <a href="http://code4kenya.org">Code4Kenya</a> data journalism project to demonstrate how data-driven tools can help ordinary citizens decipher and then act on the news they read / watch, by finding out how a national event such as the elections affects their personal lives or local communities.
+</p>
+<p>
+The <a href="http://gottovote.code4kenya.org/">first version</a> of <b>GotToVote</b>, was originally built in just 24hrs, at zero cost, to help voters register for the elections. <a href="http://code4kenya.org">Code4Kenya</a> initiated the project after citizens complained that the official data released in PDF format by the the <a href="http://www.iebc.or.ke/">Independent Electoral and Boundaries Commission (IBEC)</a> was too large to download and too cumbersome to use.
+</p>
+“The file was so big, it would have taken days for ordinary Kenyans to download. And, once they opened the document all they would have found was complex lists and tables of constituency registration centers. These elections are a tight race, and every vote counts. So, we knew that the information was simply too important to be ignored,” explains <a href="<a href="http://openinstitute.com/">OpenInstitute</a>">Open Institute</a> executive director, Jay Bhalla.
+</p><p>
+Two <a href="http://code4kenya.org">Code4Kenya</a> fellows, David Lemayian and Simeon Oriko, therefore scraped the data out of the IBEC document and built a simple website for citizens to find their registration center at the click of a button. The website also helped citizens understand the often complex procedures for registering. The site went viral.
+</p><p>
+These early successes prompted Dutch human rights organisation, <a href="http://www.hivos.org/">HiVOS</a>, to make a grant to the project to build additional functionality, as well as to package <b>GotToVote</b> and other useful civic technology tools into a ‘plug-&-play’ resource kit for media and citizen groups in other countries.
+</p><p>
+“The media and political parties are all focused on the national results and on results affecting two or three of the higher profile presidential contenders. The ordinary citizen and their need to know about their local results was getting lost in all the hype. <b>GotToVote</b> seeks to change that, by giving citizens their local results, as soon as they’re available, and mashing up these results with other locally relevant data,” says Bhalla.
+</p><p>
+The second version of <b>GotToVote</b> does two thing: it initially helps citizens spread messages of peace, by allowing them to send free SMS messages to friends and associates urging restraint at the ballot box. Once balloting closes, <b>GotToVote</b> will allow citizens to cut through all the ‘noise’ and hype generated during elections, by giving easy access to the official election results for their local counties, or local constituencies. <b>GotToVote</b> will also contextualise the results, by overlaying ballot returns with information about local trends, and official reports of local election fraud or other irregularities.
+</p><p>
+“The approach is something that data journalists in programmes like <a href="http://code4kenya.org">Code4Kenya</a> do all the time. Thousands of Kenyans were using <b>GotToVote</b> to register for the elections, within hours of the 1st version of the site going live. It proved that the real power of civic technologies is their ability to quickly and cheaply translate complex data into ‘actionable’ information, and to then calibrate the information to a citizens’ exact location or other circumstances,” says <a href="http://africanmediainitiative.org/">Africa Media Initiative</a>  digital strategist, Justin Arenstein.
+</p><p>
+<a href="http://code4kenya.org">Code4Kenya</a> is a pilot programme spearheaded by the <a href="http://africanmediainitiative.org/">Africa Media Initiative</a> and World Bank. It embedded four Data Fellows into major Kenyan newsrooms and a civil society organisation, for five months, to help kickstart experimentation with data-driven civic engagement tools. The Data Fellows were supported by an external software development team. The program is implemented by Kenya-based think tank and civic innovation lab, the <a href="http://openinstitute.com/">Open Institute</a>.
+</p><p>
+<a href="http://code4kenya.org">Code4Kenya</a> has inspired similar initiatives in Ghana and South Africa. Its success has prompted AMI to pledge additional funding to allow <a href="http://code4kenya.org">Code4Kenya</a>'s jAccelerator lab to continue building civic tech projects until the end of 2013.
+</p>
+All the data used on <b>GotToVote</b>! is freely available on another <a href="http://code4kenya.org">Code4Kenya</a> project: the AfricaOpenData portal, which is the continent's largest repository of public data, ranging from government budget and tender information, to data about parliamentarians and other public officials.
+
         			<br>
         			<p class="lead" style="text-align: center;"><b><em>We are Open!</em></b></p>
         			<p style="text-align: center;">
