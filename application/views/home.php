c0590254a6f31bc05466aa8c8401b080822f44e7
David Lemayian <david@on.co.ke>
2013-03-06 23:44:17 +0300

    Changed IEBC gadget position.

--- application/views/home.php
+++ application/views/home.php
@@ -123,61 +123,11 @@
 	</div>
 </div>
 
-<div class="container">
+<div style="width:1025px;margin:auto;">
+	<iframe src="http://vote.iebc.or.ke/results/" width="1024px" height="655" frameborder="0"></iframe>
+</div>
 
-	<div class="jumbotron" id="jumbotron" style="display:none;">
-		
-		<div class="clock">
-			<!-- Days -->
-			<div class="clock_days">
-			    <div class="bgLayer">
-			        <canvas id="canvas_days" width="122" height="122">
-			            Your browser does not support the HTML5 canvas tag.
-			        </canvas>
-			        <p class="val">0</p>
-			        <p class="type_days">Days</p>
-			    </div>
-			</div>
-			<!-- /Days -->
-			<!-- Hours -->
-			<div class="clock_hours">
-				<div class="bgLayer">
-					<canvas id="canvas_hours" width="122" height="122">
-						Your browser does not support the HTML5 canvas tag.
-					</canvas>
-					<p class="val">0</p>
-					<p class="type_hours">Hours</p>
-				</div>
-			</div>
-			<!-- /Hours -->
-			<!-- Minutes -->
-			<div class="clock_minutes">
-			    <div class="bgLayer">
-			        <canvas id="canvas_minutes" width="122" height="122">
-			            Your browser does not support the HTML5 canvas tag.
-			        </canvas>
-			        <div class="text">
-			            <p class="val">0</p>
-			            <p class="type_minutes">Minutes</p>
-			        </div>
-			    </div>
-			</div>
-			<!-- /Minutes -->
-			<!-- Seconds -->
-			<div class="clock_seconds">
-			    <div class="bgLayer">
-			        <canvas id="canvas_seconds" width="122" height="122">
-			            Your browser does not support the HTML5 canvas tag.
-			        </canvas>
-			        <p class="val">0</p>
-			        <p class="type_seconds">Seconds</p>
-			    </div>
-			</div>
-			<!-- /Seconds -->
-		</div>
-		<div class="clearfix"></div>
-		
-	</div>
+<div class="container">
 	
 	<a name="find" id="find"></a>
 	
@@ -416,10 +366,7 @@
 
 	</div>
 </div>
-	<div style="width:1025px;margin:auto;">
-<div><iframe src="http://vote.iebc.or.ke/results/" width="1024px" height="650" frameborder="0"></iframe>
-	</div>
-</div>
+
 
 <!-- Scripts -->
 
