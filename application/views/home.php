8d3ed4705ae5a416a2e628ff79af8840c16a5ece
Nick Hargreaves <hargreaves1234@gmail.com>
2013-03-07 12:15:14 +0300

    open report page in new tab

--- application/views/home.php
+++ application/views/home.php
@@ -12,14 +12,14 @@
 					</li>
 					<li style='margin-bottom:10px;' class="dropdown"><a class="btn btn-large btn-success homebtn dropdown" href="#" class="dropdown-toggle" id="drop_report" role="button" data-toggle="dropdown" target='_blank'><i class="icon-bullhorn icon-white"></i> Make a Report</a>
 						<ul id="menu2" class="dropdown-menu" role="menu" aria-labelledby="drop_report">
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="https://uchaguzi.co.ke/reports/submit">Uchaguzi</a></li>
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="http://ushahidi.voiceofmathare.org/index.php/reports/submit">Voice of Mathare</a></li>
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="http://www.nscpeace.go.ke/108/report.php">Amani Kenya @ 108</a></li>
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="http://www.iebc.or.ke/report-to-us/">IEBC Whistle Blowing Portal</a></li>
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="http://www.sisiniamani.org/">Sisi ni Amani</a></li>
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="http://www.kenyapolice.go.ke/report_a_crime.asp">Kenya Police</a></li>
-		                <li role="presentation"><a role="menuitem" tabindex="-1" href="http://www.knchr-idp.org/">KNHRC</a></li>
-		                </ul>	
+		                  <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="https://uchaguzi.co.ke/reports/submit">Uchaguzi</a></li>
+		                <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="http://ushahidi.voiceofmathare.org/index.php/reports/submit">Voice of Mathare</a></li>
+		                <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="http://www.nscpeace.go.ke/108/report.php">Amani Kenya @ 108</a></li>
+		                <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="http://www.iebc.or.ke/report-to-us/">IEBC Whistle Blowing Portal</a></li>
+		                <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="http://www.sisiniamani.org/">Sisi ni Amani</a></li>
+		                <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="http://www.kenyapolice.go.ke/report_a_crime.asp">Kenya Police</a></li>
+		                <li role="presentation"><a target="_blank" role="menuitem" tabindex="-1" href="http://www.knchr-idp.org/">KNHRC</a></li>
+		            </ul>	
 	                </li>
 	                <li style='margin-bottom:10px;'>
 						<a class="btn btn-large btn-success homebtn" href="http://gottovote.code4kenya.org/" target='_blank'><i class="icon-search icon-white"></i> Registration Centers</a>
